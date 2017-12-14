33d03427273203dd01bc88ef11e70878bca6205e
Josh Lehman <jalehman37@gmail.com>
2017-12-14 15:16:43 -0800

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject starcity/toolbelt-date "0.1.0-SNAPSHOT"
+(defproject starcity/toolbelt-date "0.1.0"
   :description "Utilities for working with dates in Clojure/Script."
   :url "https://github.com/starcity-properties/toolbelt-date"
   :license {:name "Eclipse Public License"
