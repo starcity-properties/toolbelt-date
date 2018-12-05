791809e0c67286ce881d3e8b6c8bb78b28e9c083
Diana Negma <dianagren@gmail.com>
2018-12-05 07:53:40 -0800

    Add min/max function for dates, change repositories to our own starjars. (#6)

--- test/toolbelt/date_test.clj
+++ test/toolbelt/date_test.clj
@@ -98,14 +98,23 @@
            (date/plus date (date/period :millis 100))))))
 
 
+;; =============================================================================
+;; Property based
+;; =============================================================================
+
+
+(defn- gen-date* []
+  (gen/fmap (fn [n] (c/to-date n)) gen/int))
+
+
 (defspec date-plus-minus-test
+  ;; Run test 100 times with different generated data
   100
   (prop/for-all
-    [year (gen/double* {:min 1 :max 3000 :NaN? false})
-     month (gen/double* {:min 1 :max 12 :NaN? false})
-     p gen/pos-int]
-    (let [date-time (t/date-time (int year) (int month))
-          inst      (c/to-date date-time)
+    ;; Generate random date and integer to represent number of days
+    [inst (gen-date*)
+     p gen/int]
+    (let [date-time (c/to-date-time inst)
           days      (t/days p)]
       (testing "The inst plus a period is always equal that inst minus the same period."
         (are [d2] (= inst d2)
@@ -116,5 +125,19 @@
           (-> (date/plus inst days)
               (date/minus days))
           ;; Long (unix time)
-          (-> (date/plus (c/to-long date-time) days)
+          (-> (date/plus (c/to-long inst) days)
               (date/minus days)))))))
+
+
+(defspec date-min-max-test
+  ;; Run test 100 times with different generated data
+  100
+  (prop/for-all
+    ;; Generate a vector of dates with 1 to 100 elements
+    [dts (gen/vector (gen-date*) 1 100)]
+    (testing "Min date is always the earliest and max date is always the latest."
+      (let [sorted-dates (sort dts)]
+        (is (= (apply date/max dts) (last sorted-dates))
+            "Max date should be the latest date.")
+        (is (= (apply date/min dts) (first sorted-dates))
+            "Min date should be the earliest date.")))))
